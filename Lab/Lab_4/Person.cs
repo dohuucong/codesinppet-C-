4485a726ae697a0ddded739d9e53078cdb482598
dohuucong <60845588+dohuucong@users.noreply.github.com>
2020-06-21 23:26:08 +0700

    first commit

---
+++ Lab/Lab_4/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn_CSharp_DotNET.Lab.Lab_4
+{
+    abstract class Person
+    {
+        protected string Name;
+        protected int Phone;
+        protected string Email;
+        protected Person()
+        {
+            Name = "Huy";
+            Phone = 0964576059;
+            Email = "VQH@gmail.com";
+        }
+        protected Person(string Name,int Phone,string Email)
+        {
+            this.Name = Name;
+            this.Phone = Phone;
+            this.Email = Email;
+        }
+
+        public string PName
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        public int PPhone
+        {
+            get { return Phone; }
+            set { Phone = value; }
+        }
+        public string PEmail
+        {
+            get { return Email; }
+            set { Email = value; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿//using Learn_CSharp_DotNET.Book.Session_7;
+//using Learn_CSharp_DotNET.lession.lessdion3.Students;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Learn_CSharp_DotNET
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //Book
+//            //Book.Session_3.Run.Test();
+//            //Book.Session_4.Run.Test();
+//            //Book.Session_5.Run.Test();
+//            //Book.Session_6.Run.Test();
+
+
+//            //CodeLean
+//            //CodeLean.AP_03.Run.Test();
+//            //CodeLean.AP_04.Run.Test();
+//            //CodeLean.AP_05.Run.Test();
+//            //CodeLean.AP_06.Run.Test();
+
+//            //lession.lessdion3.Students.Run.Test();
+
+//            //Lab.Lab_3.Run.Test();
+
+//            //Book.Session_7.Run.Test();
+
+//            //Book.Session_8.Run.Test();
+
+//            //Book.Session_9.Run.Test();
+
+//            //Reseach.OOP.Run.Test();
+
+//            //Book.Session_10.Run.Test();
+
+//            //Book.Session_11.Run.Test();
+
+
+//            CodeLean.AP_10.Run.Test();
+//            //Lab.Lab_4.Run.Test();
+
+//            //Pause:
+//            Console.ReadKey();
+//        }
+//    }
+//}
